217b6d60ba6117589e1046f0dd56de09b7a21c86
lillepeder <plillebostad@gmail.com>
2020-12-15 11:53:18 +0100

    fixed naming mistake

--- F_Machine_learning/1_Unsupervised-Learning/solutions/3_1.py
+++
@@ -1,4 +0,0 @@
-# resize according to smallest image
-sz = imgs[smallest_index].size
-im1 = im1.resize(sz)
-im2 = im2.resize(sz)
--- F_Machine_learning/1_Unsupervised-Learning/solutions/ex3_1.py
+++ F_Machine_learning/1_Unsupervised-Learning/solutions/ex3_1.py
@@ -1,13 +1,4 @@
-# mixing matrix
-np.random.seed(42)
-A = np.random.randn(4,3)
-
-print(A.T)
-
-# apply transformation
-X = S @ A.T
-
-# EXPLANATION:
-# each column in A.T defines one of the linear combinations (4 in total)
-# the rows define how much to add from each source. 
-# E.g. the first combination will be (0.5*im1 - 0.14*im2 + 0.65*im3)
+# resize according to smallest image
+sz = imgs[smallest_index].size
+im1 = im1.resize(sz)
+im2 = im2.resize(sz)
