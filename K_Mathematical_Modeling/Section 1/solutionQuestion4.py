ced21bf99513c3175c991f566b3623d72baea35a
sylvaintollis <72555200+sylvaintollis@users.noreply.github.com>
2021-02-11 09:18:49 +0200

    Add files via upload

---
+++ K_Mathematical_Modeling/Section 1/solutionQuestion4.py
@@ -0,0 +1,3 @@
+from IPython.display import Image
+img = 'solutionQuestion4.png'
+Image(url=img, width=1000, height=1000)
---
+++ K_Mathematical_Modeling/Section 1/solutionQuestion6.py
@@ -0,0 +1,1 @@
+print("we could use model a, or model b. We now know that model a would give a wrong estimate, because it ignores the branching geometry of the actin filaments at the leading edge, and this is an important parameter as demonstrated by model b.")
